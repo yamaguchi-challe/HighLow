b86d3ce7c812adc9e9c12f4034d0e27d59f25580
yamaguchi-challe <gs.yamaguchi111@gmail.com>
2024-07-22 20:30:52 +0900

    アニメーションの追加

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 let card = 1;// 場にあるトランプのカード番号
 let Result = "";//勝ち負けの結果
+let changed = false; //カードが反転しているフラグ
 
 $("#next").css("display", "none"); //nextボタンの非表示
 
 // Highがクリックされたときの処理
 $("#high").on("click", function(){
-    document.getElementById("Card_img_Before").src="./img/card" + card + ".png";
     let trump_n = Math.ceil( Math.random() * 13);// ランダムで数字を決める
+    changed = false; //カード反転フラグをリセット
 
     if (card < trump_n) {//賭けカードが、伏せカードより大きい場合
         // 勝ち
-        Result = "<span style='background:red;'>【 勝ち 】</span>";
+        Result = "<span style='background:red;'>WIN!!</span>";
     }
     else if (card > trump_n) {//賭けカードが、伏せカードより小さい場合
         // 負け
-        Result = "<span style='background:blue;'>『 負け 』</span>";
+        Result = "<span style='background:blue;'>LO$E...</span>";
     }
     else {
         // 引き分け
@@ -25,16 +26,16 @@ $("#high").on("click", function(){
 
 // Lowがクリックされたときの処理
 $("#low").on("click", function(){
-    document.getElementById("Card_img_Before").src="./img/card" + card + ".png";
     let trump_n = Math.ceil( Math.random() * 13);// ランダムで数字を決める
+    changed = false; //カード反転フラグをリセット
 
     if (card < trump_n) {//賭けカードが、伏せカードより大きい場合
         // 負け
-        Result = "<span style='background:blue;'>『 負け 』</span>";
+        Result = "<span style='background:blue;'>LO$E...</span>";
     }
     else if (card > trump_n) {//賭けカードが、伏せカードより小さい場合
         // 勝ち
-        Result = "<span style='background:red;'>【 勝ち 】</span>";
+        Result = "<span style='background:red;'>WIN!!</span>";
     }
     else {
         // 引き分け
@@ -44,20 +45,59 @@ $("#low").on("click", function(){
 });
 
 function showResult(trump_n) {
-    document.getElementById("Card_img_After").src="./img/card" + trump_n + ".png";
-    document.getElementById("After").innerHTML = Result;
-    document.getElementById("Before").innerHTML ="場にあるカードは" + card ;
+    rotationAnimationLoop(document.getElementById("afterCard"), "./img/card" + trump_n + ".png", 0);
+    setTimeout(function(){
+        $("#after").fadeOut(0).fadeIn(500).html(Result);
+      },700);  
+    
+    
     card = trump_n;//場にあるカードを新しく引いたカードに変える
 
     $("#game").css("display", "none");
-    $("#next").css("display", "block");
+    setTimeout(function(){
+        $("#next").css("display", "block");
+      },1000);   
+    
 }
 
-function Next(){
-    document.getElementById("Card_img_Before").src="./img/card" + card + ".png";
-    document.getElementById("Card_img_After").src="./img/card_back.png";
-    document.getElementById("Before").innerHTML ="場にあるカードは" + card ;
-    document.getElementById("After").innerHTML ="次のカードは今の数字より高い？低い？"
+// 次へがクリックされたときの処理
+$("#next").on("click", function(){
+    $("#beforeCard").attr("src", "./img/card" + card + ".png");
+    $("#afterCard").attr("src", "./img/card_back.png");
+    $("#after").html("次のカードは今の数字より高い？低い？");
     $("#game").css("display", "block");
     $("#next").css("display", "none");
+});
+
+const rotationAnimationLoop = (element, imagePath, deg) =>{
+    if(deg>=90){
+        changed = true;
+    }
+    if(!changed){
+        if( deg < 90 ){
+            rotationAnimation(element, imagePath, deg);
+            setTimeout( 
+                () => {
+                    rotationAnimationLoop(element, imagePath, deg+= 5 )
+                }, 
+            25);
+        } 
+    } else{
+        if(deg>0){
+            rotationAnimation(element, imagePath, deg);
+            setTimeout( 
+                () => {
+                    rotationAnimationLoop(element, imagePath, deg-= 5 )
+                }, 
+            25);
+        }
+    }
+}
+
+const rotationAnimation = (element, imagePath, deg) =>{
+    if ( 90 == deg ){
+        element.src =  imagePath;
+    }else {
+        element.style.webkitTransform = 'rotateY(' + deg + 'deg)';
+    }
 }
